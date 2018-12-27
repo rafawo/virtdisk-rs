09e86ab9d789b5c3f9085317e23da14b236038f8
Rafael Alcaraz Mercado <ramerc@microsoft.com>
2018-12-26 22:37:24 -0800

    Implement try_get_disk_volume_path

--- src/diskutilities.rs
+++ src/diskutilities.rs
@@ -155,27 +155,52 @@ impl Disk {
     }
 }
 
+struct Volume {
+    handle: Handle,
+}
+
+impl std::ops::Drop for Volume {
+    fn drop(&mut self) {
+        crate::win_wrappers::close_handle(&mut self.handle);
+    }
+}
+
+impl Volume {
+    pub fn open(path: &str, access_mask: Option<DWord>) -> Result<Volume, ResultCode> {
+        use winapi::um::{fileapi, winnt};
+
+        let access_mask_flags = match access_mask {
+            Some(flags) => flags,
+            None => winnt::GENERIC_READ | winnt::GENERIC_WRITE,
+        };
+
+        match crate::win_wrappers::create_file(
+            path,
+            access_mask_flags,
+            winnt::FILE_SHARE_READ | winnt::FILE_SHARE_WRITE,
+            None,
+            fileapi::OPEN_EXISTING,
+            winnt::FILE_ATTRIBUTE_NORMAL,
+            None,
+        ) {
+            Ok(handle) => Ok(Volume { handle }),
+            Err(error) => Err(error),
+        }
+    }
+}
+
 /// Force a volume to be brought online (ie: mounted by a filesystem).
 /// This is needed when automount has been disabled (mountvol /N).
 pub fn force_online_volume(volume_name: &str) -> Result<(), ResultCode> {
-    use winapi::um::{fileapi, ioapiset, winioctl, winnt};
+    use winapi::um::{ioapiset, winioctl};
 
-    match crate::win_wrappers::create_file(
-        volume_name,
-        winnt::GENERIC_READ | winnt::GENERIC_WRITE,
-        winnt::FILE_SHARE_READ | winnt::FILE_SHARE_WRITE,
-        None,
-        fileapi::OPEN_EXISTING,
-        winnt::FILE_ATTRIBUTE_NORMAL,
-        None,
-    ) {
-        Ok(handle) => {
+    match Volume::open(volume_name, None) {
+        Ok(volume) => {
             let mut bytes: DWord = 0;
-            let mut handle = handle;
 
             unsafe {
-                match ioapiset::DeviceIoControl(
-                    handle,
+                if ioapiset::DeviceIoControl(
+                    volume.handle,
                     winioctl::IOCTL_VOLUME_OFFLINE,
                     std::ptr::null_mut(),
                     0,
@@ -183,39 +208,157 @@ pub fn force_online_volume(volume_name: &str) -> Result<(), ResultCode> {
                     0,
                     &mut bytes,
                     std::ptr::null_mut(),
-                ) {
-                    0 => {
-                        match ioapiset::DeviceIoControl(
-                            handle,
-                            winioctl::IOCTL_VOLUME_ONLINE,
-                            std::ptr::null_mut(),
-                            0,
-                            std::ptr::null_mut(),
-                            0,
-                            &mut bytes,
-                            std::ptr::null_mut(),
-                        ) {
-                            0 => {
-                                crate::win_wrappers::close_handle(&mut handle);
-                                Ok(())
-                            }
-                            _ => {
-                                crate::win_wrappers::close_handle(&mut handle);
-                                Err(error_code_to_result_code(
-                                    winapi::um::errhandlingapi::GetLastError(),
-                                ))
-                            }
-                        }
-                    }
-                    _ => {
-                        crate::win_wrappers::close_handle(&mut handle);
-                        Err(error_code_to_result_code(
-                            winapi::um::errhandlingapi::GetLastError(),
-                        ))
-                    }
+                ) == 0
+                {
+                    return Err(error_code_to_result_code(
+                        winapi::um::errhandlingapi::GetLastError(),
+                    ));
                 }
+
+                if ioapiset::DeviceIoControl(
+                    volume.handle,
+                    winioctl::IOCTL_VOLUME_ONLINE,
+                    std::ptr::null_mut(),
+                    0,
+                    std::ptr::null_mut(),
+                    0,
+                    &mut bytes,
+                    std::ptr::null_mut(),
+                ) == 0
+                {
+                    return Err(error_code_to_result_code(
+                        winapi::um::errhandlingapi::GetLastError(),
+                    ));
+                }
+
+                Ok(())
             }
         }
         Err(error) => Err(error),
     }
+}
+
+#[repr(C)]
+#[allow(dead_code)]
+struct StorageDeviceNumber {
+    device_type: u32,
+    device_number: DWord,
+    partition_number: DWord,
+}
+
+#[allow(dead_code)]
+struct SafeFindVolumeHandle {
+    handle: Handle,
+}
+
+impl std::ops::Drop for SafeFindVolumeHandle {
+    fn drop(&mut self) {
+        if self.handle == std::ptr::null_mut() {
+            return;
+        }
+
+        #[allow(unused_assignments)]
+        let mut result: Bool = 0;
+
+        unsafe {
+            result = winapi::um::fileapi::FindVolumeClose(self.handle);
+        }
+
+        match result {
+            result if result == 0 => {
+                panic!("Closing handle failed with error code {}", unsafe {
+                    winapi::um::errhandlingapi::GetLastError()
+                });
+            }
+            _ => {}
+        }
+    }
+}
+
+/// Tries to get the volume path of the volume in a disk.
+fn try_get_disk_volume_path(handle: Handle) -> Result<String, ResultCode> {
+    use winapi::um::{fileapi, ioapiset, winioctl};
+
+    let mut dev_number = StorageDeviceNumber {
+        device_type: 0,
+        device_number: 0,
+        partition_number: 0,
+    };
+
+    let mut bytes: DWord = 0;
+
+    unsafe {
+        if ioapiset::DeviceIoControl(
+            handle,
+            winapi::um::winioctl::IOCTL_STORAGE_GET_DEVICE_NUMBER,
+            std::ptr::null_mut(),
+            0,
+            &mut dev_number as *mut _ as LPVoid,
+            std::mem::size_of::<StorageDeviceNumber>() as DWord,
+            &mut bytes,
+            std::ptr::null_mut(),
+        ) == 0
+        {
+            return Err(error_code_to_result_code(
+                winapi::um::errhandlingapi::GetLastError(),
+            ));
+        }
+
+        const MAX_PATH: usize = 256;
+        let mut volume_name_buffer: [WChar; MAX_PATH] = [0; MAX_PATH];
+        let find_volume_handle =
+            fileapi::FindFirstVolumeW(volume_name_buffer.as_mut_ptr(), MAX_PATH as DWord);
+
+        if find_volume_handle == std::ptr::null_mut() {
+            return Err(error_code_to_result_code(
+                winapi::um::errhandlingapi::GetLastError(),
+            ));
+        }
+
+        let find_volume = SafeFindVolumeHandle {
+            handle: find_volume_handle,
+        };
+
+        loop {
+            let volume_name_wstr =
+                widestring::WideCString::from_ptr_str(volume_name_buffer.as_ptr());
+            let mut volume_name = volume_name_wstr.to_string_lossy();
+
+            if volume_name.chars().last().unwrap() == '\\' {
+                volume_name.pop();
+                volume_name.push('\0');
+            }
+
+            if let Ok(volume) = Volume::open(&volume_name, Some(0)) {
+                let mut extents = std::mem::zeroed::<winioctl::VOLUME_DISK_EXTENTS>();
+
+                if ioapiset::DeviceIoControl(
+                    volume.handle,
+                    winioctl::IOCTL_VOLUME_GET_VOLUME_DISK_EXTENTS,
+                    std::ptr::null_mut(),
+                    0,
+                    &mut extents as *mut _ as LPVoid,
+                    std::mem::size_of::<winioctl::VOLUME_DISK_EXTENTS>() as DWord,
+                    &mut bytes,
+                    std::ptr::null_mut(),
+                ) == 0
+                {
+                    if extents.Extents[0].DiskNumber == dev_number.device_number {
+                        return Ok(volume_name);
+                    }
+                }
+            }
+
+            if fileapi::FindNextVolumeW(
+                find_volume.handle,
+                volume_name_buffer.as_mut_ptr(),
+                MAX_PATH as DWord,
+            ) == 0
+            {
+                break;
+            }
+        }
+    }
+
+    Err(ResultCode::FileNotFound)
 }
