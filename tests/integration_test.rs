ce021e850226bf2226f91f58adabc5772d5c857a
Rafael Alcaraz Mercado <ramerc@microsoft.com>
2019-01-03 01:17:25 -0800

    Add expand_volume test case

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -70,3 +70,24 @@ fn can_expand_vhd() {
     let vhd = open_vhd(&disk_path, false).unwrap();
     assert!(expand_vhd(&vhd, 50 * 1024 * 1024 * 1024).unwrap());
 }
+
+#[test]
+fn can_expand_volume() {
+    let disk_path = String::from(format!(
+        "base_{}.vhdx",
+        FILE_ID.fetch_add(1, Ordering::SeqCst)
+    ));
+    let _delete_file_scope_exit = DeleteDiskScopeExit {
+        filepath: &disk_path,
+    };
+
+    let mounted_volume = create_base_vhd(&disk_path, 20, 32, "NTFS").unwrap();
+    drop(mounted_volume);
+
+    let vhd = open_vhd(&disk_path, false).unwrap();
+    assert!(expand_vhd(&vhd, 50 * 1024 * 1024 * 1024).unwrap());
+    assert_eq!((), mount_vhd_temporarily_for_setup(&vhd).unwrap());
+
+    let disk = open_vhd_backed_disk(&vhd).unwrap();
+    assert!(disk.expand_volume().unwrap());
+}
